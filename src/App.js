0c9edf16d9d76c8ae46c822af90c6d999ea29dc7
Ahmed Merehil <ahmed8merehil@yahoo.com>
2021-03-20 18:04:04 +0000

    created product card component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import CssBaseLine from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
+import Container from '@material-ui/core/Container';
 import Home from './pages/Home';
 
+const useStyles = makeStyles({
+  root: {
+    padding: 20,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 const App = () => {
+  const classes = useStyles();
+
   return (
   <Router>
     <CssBaseLine/>
     <NavBar/>
+    <Container className={classes.root}>
     <Switch>
       <Route path="/">
        <Home />
       </Route>
     </Switch>
+    </Container>
   </Router>
   )  
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import Grid from '@material-ui/core/Grid';
+import ProductCard from '../components/ProductCard';
 
 const  Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Grid container>
+            <Grid xs={12} md={4} item> <ProductCard/> </Grid>
+            <Grid xs={12} md={4} item> <ProductCard/> </Grid>
+            <Grid xs={12} md={4} item> <ProductCard/> </Grid>
+            
+        </Grid>
+        // </Container>
     )
 }
 
